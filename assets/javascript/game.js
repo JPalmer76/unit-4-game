9fc3a36dc3dcfeba32799a3a007d65085aa283a6
Jonny <jpdadof3@gmail.com>
2019-05-12 20:06:11 -0500

    added images, and basic layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// computer selects a random number between 19-120
+var targetNum = Math.floor(Math.random() * 6) 
+
+$("#randNum").text(targetNum);
+
+// each crystal is a button with a set number value
