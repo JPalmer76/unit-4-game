535e8ad46ee762a41e0a89297274e03123fb4256
Jonny <jpdadof3@gmail.com>
2019-05-14 18:58:13 -0500

    added random number selector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,17 @@
-// computer selects a random number between 19-120
-var targetNum = Math.floor(Math.random() * 6) 
+$(document).ready(function() {
 
-$("#randNum").text(targetNum);
+    var targetNumber = Math.floor(Math.random() * 108) + 19
+    $('#random-num').append(targetNumber);
 
-// each crystal is a button with a set number value
+    var wins = 0;
+    if ('#' === (targetNumber)){
+
+    }
+
+    var purpleCrystal = 10;
+    $('#click1').append('#final');
+    
+    
+    
+
+})
