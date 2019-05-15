bc0811c7c819e8a7a0039e0c314447396e28633f
Jonny <jpdadof3@gmail.com>
2019-05-15 17:56:42 -0500

    wrote random number logic for crystals. wins and losses are tracking. so far everything is functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,47 @@
 $(document).ready(function() {
+  var wins = 0;
+  var loses = 0;
+  var playerTotal = 0;
+  var targetNumber = Math.floor(Math.random() * 102 + 19);
+  $("#random-num").append(targetNumber);
 
-    var targetNumber = Math.floor(Math.random() * 108) + 19
-    $('#random-num').append(targetNumber);
+  var purpleCrystal = Math.floor(Math.random() * 12 + 1);
 
-    var wins = 0;
-    if ('#' === (targetNumber)){
+  $("#purple").on("click", function() {
+    playerTotal = playerTotal + purpleCrystal;
+    $("#final").text(playerTotal);
+    winChecker()
+  });
+  var orangeCrystal = Math.floor(Math.random() * 12 + 1);
+  $("#orange").on("click", function() {
+    playerTotal = playerTotal + orangeCrystal;
+    $("#final").text(playerTotal);
+    winChecker()
+  });
 
-    }
+  var glassCrystal = Math.floor(Math.random() * 12 + 1);
+  $("#glass").on("click", function() {
+    playerTotal = playerTotal + glassCrystal;
+    $("#final").text(playerTotal);
+    winChecker()
+  });
 
-    var purpleCrystal = 10;
-    $('#click1').append('#final');
-    
-    
-    
+  var blueCrystal = Math.floor(Math.random() * 12 + 1);
+  $("#blue").on("click", function() {
+    playerTotal = playerTotal + blueCrystal;
+    $("#final").text(playerTotal);
+    winChecker()
+  });
+  
+  function winChecker() {
+  if (playerTotal === targetNumber){
+      wins = wins + 1;
+      $('#won').text('wins: ' + wins);
+      alert('You Win !!')
+  }else if (playerTotal > targetNumber) {
+    loses = loses + 1;
+    $('#lose').text('losses: ' + loses);
+      alert("You Lose !!")
 
-})
+    }}
+});
